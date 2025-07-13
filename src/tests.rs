d31bb9f2f47c1698311f7e5c1a5cd75e87b9819e
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-07-12 21:00:23 -0400

    feat: add * and / functions

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,7 @@
-use crate::types::Env;
-
 #[allow(dead_code)]
 #[test]
 fn test_add_valid_input() {
-    use crate::{parser::parse_eval, types::Expr};
+    use crate::{parser::parse_eval, types::{Env, Expr}};
     let env = &mut Env::default_env();
     let input = "(+ 1 1)".to_string();
     let result = parse_eval(input, env);
@@ -13,9 +11,29 @@ fn test_add_valid_input() {
 #[allow(dead_code)]
 #[test]
 fn test_sub_valid_input() {
-    use crate::{parser::parse_eval, types::Expr};
+    use crate::{parser::parse_eval, types::{Env, Expr}};
     let env = &mut Env::default_env();
     let input = "(- 1 1)".to_string();
     let result = parse_eval(input, env);
     assert!(matches!(result, Ok(Expr::Number(0))));
+}
+
+#[allow(dead_code)]
+#[test]
+fn test_mult_valid_input() {
+    use crate::{parser::parse_eval, types::{Env, Expr}};
+    let env = &mut Env::default_env();
+    let input = "(* 1 2)".to_string();
+    let result = parse_eval(input, env);
+    assert!(matches!(result, Ok(Expr::Number(2))));
+}
+
+#[allow(dead_code)]
+#[test]
+fn test_div_valid_input() {
+    use crate::{parser::parse_eval, types::{Env, Expr}};
+    let env = &mut Env::default_env();
+    let input = "(/ 4 2)".to_string();
+    let result = parse_eval(input, env);
+    assert!(matches!(result, Ok(Expr::Number(2))));
 }
--- src/types.rs
+++ src/types.rs
@@ -74,6 +74,42 @@ impl Env {
                 }
             )
         );
+        data.insert(
+            "*".to_string(), 
+            Expr::Func(
+                |args: &[Expr]| -> Result<Expr, Error> {
+                    let numbers = parse_number_list(args)?; 
+
+                    let first = *numbers
+                        .first()
+                        .ok_or(
+                            Error::Message("expected at least one number".to_string())
+                        )?;
+
+                    let sum_of_rest = numbers[1..].iter().fold(0, |sum, a| sum + a);
+
+                    Ok(Expr::Number(first * sum_of_rest))
+                }
+            )
+        );
+        data.insert(
+            "/".to_string(), 
+            Expr::Func(
+                |args: &[Expr]| -> Result<Expr, Error> {
+                    let numbers = parse_number_list(args)?; 
+
+                    let first = *numbers
+                        .first()
+                        .ok_or(
+                            Error::Message("expected at least one number".to_string())
+                        )?;
+
+                    let sum_of_rest = numbers[1..].iter().fold(0, |sum, a| sum + a);
+
+                    Ok(Expr::Number(first / sum_of_rest))
+                }
+            )
+        );
 
         Env {data}
     }
