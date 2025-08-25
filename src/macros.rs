68e8f21c0240092454e7fa0dcebf042a73d722f8
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-08-25 15:49:37 -0400

    feat: refactor define to allow implicit lambdas
    Refactored define macro to handle lambda functions that don't use the lambda keyword. Ex. `(define (example x) (println x))`.

--- src/macros.rs
+++ src/macros.rs
@@ -5,30 +5,36 @@
 //! Define functions and variables.
 
 use crate::env::Env;
-use crate::{error::Error, types::Expr, types::Closure};
 use crate::parser::eval;
+use crate::{error::Error, types::Closure, types::Expr};
 
+use std::cell::RefCell;
 use std::rc::Rc;
-use std::cell::RefCell; 
-
 
 /// Associate a symbol with a value in an environment.
 pub fn define(args: &[Expr], env: Rc<RefCell<Env>>) -> Result<Expr, Error> {
-    let mut iter = args.iter();
-    let name = match iter.next() {
-        Some(Expr::Symbol(n)) => n.to_owned(),
-        _ => return Err(Error::Message("expected symbol name".to_string())),
-    };
+    match (args.get(0), args.get(1)) {
+        (Some(Expr::Symbol(name)), Some(expr)) => {
+            let value = eval(&expr, env.clone())?;
+            env.borrow_mut().data.insert(name.to_owned(), value);
+        }
+        (Some(Expr::List(l)), Some(_)) => {
+            // Get name + remove it from l.
+            let name = match l.first() {
+                Some(Expr::Symbol(s)) => s,
+                _ => {
+                    return Err(Error::Message("ill-formed special form name".to_string()));
+                }
+            };
 
-    let expr = match iter.next() {
-        Some(e) => e.clone(),
-        _ => return Err(Error::Message("invalid data".to_string())),
-    };
-
-    let value = eval(&expr, env.clone())?;
-
-    env.borrow_mut().data.insert(name, value);
-
+            let args_without_name = Expr::List(l.iter().skip(1).cloned().collect());
+            let value = lambda(&[args_without_name, args[1].clone()], env.clone())?;
+            env.borrow_mut().data.insert(name.to_owned(), value);
+        }
+        _ => {
+            return Err(Error::Message("ill-formed special form".to_string()));
+        }
+    }
     Ok(Expr::Void())
 }
 
@@ -43,22 +49,28 @@ pub fn lambda(args: &[Expr], env: Rc<RefCell<Env>>) -> Result<Expr, Error> {
     // Get argument symbols.
     let arg_list = match iter.next() {
         Some(Expr::List(l)) => l,
-        _ => return Err(Error::Message("malformed lambda".to_string())),
+        e => return Err(Error::Message(format!("ill-formed lambda: {:?}", e))),
     };
 
     // Add argument symbols to env.
-    let params: Vec<String> = arg_list.iter().map(|arg| {
-        if let Expr::Symbol(s) = arg {
-            Ok(s.clone())
-        } else {
-            Err(Error::Message("lambda params must be symbols".to_string()))
-        }
-    }).collect::<Result<_, _>>()?;
+    let params: Vec<String> = arg_list
+        .iter()
+        .map(|arg| {
+            if let Expr::Symbol(s) = arg {
+                Ok(s.clone())
+            } else {
+                Err(Error::Message(format!(
+                    "lambda params must be symbols: {:?}",
+                    arg
+                )))
+            }
+        })
+        .collect::<Result<_, _>>()?;
 
     // Get function.
     let body = match iter.next() {
         Some(e) => e,
-        _ => return Err(Error::Message("expected function in lambda".to_string()))
+        _ => return Err(Error::Message("expected lambda body".to_string())),
     };
 
     let closure = Box::new(Closure::init(env.clone(), params, body.clone()));
@@ -68,7 +80,9 @@ pub fn lambda(args: &[Expr], env: Rc<RefCell<Env>>) -> Result<Expr, Error> {
 /// Evaluate lambda with arguments.
 pub fn apply_lambda(closure: &Closure, args: Vec<Expr>) -> Result<Expr, Error> {
     if args.len() != closure.parameters.len() {
-        return Err(Error::Message(format!("wrong number of arguments passed to procedure")));
+        return Err(Error::Message(format!(
+            "wrong number of arguments passed to procedure"
+        )));
     }
 
     // new environment extends the closure’s captured env
@@ -81,6 +95,5 @@ pub fn apply_lambda(closure: &Closure, args: Vec<Expr>) -> Result<Expr, Error> {
         }
     }
 
-   eval(&closure.body, new_env)
+    eval(&closure.body, new_env)
 }
-
