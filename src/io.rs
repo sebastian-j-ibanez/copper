016c0035a72b5a8a5547de36f41d3e8303b855e4
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-10-29 22:48:10 -0400

    chore: update version show in repl

--- src/io.rs
+++ src/io.rs
@@ -15,7 +15,7 @@ use crate::error::Error;
 use crate::parser;
 use crate::types::Expr;
 
-pub const COPPER_VERSION: &str = "0.2.2";
+pub const COPPER_VERSION: &str = "0.2.3";
 
 /// Get expression from stdin.
 pub fn stdin_input() -> String {
